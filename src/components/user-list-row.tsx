54234c69ea680c2a8b034862d3e00e66398f2afd
leedsrising <leedsrising@gmail.com>
2023-05-22 16:12:21 -0400

    user list row component

---
+++ src/components/user-list-row.tsx
@@ -0,0 +1,42 @@
+// Import deps
+import React from 'react'
+
+// Create interfaces
+interface UserListRowUI {
+  position: number;
+  user: {
+    id: number;
+    name: string;
+    email: string;
+    service: string;
+  }
+  handleUserRemove: (id: number, title: string) => void;
+}
+
+// Create BookshelfListRow component
+export const BookshelfListRow = (props: UserListRowUI) => (
+  <tr className="table-row">
+    <td className="table-item">
+      {props.position}
+    </td>
+
+    <td className="table-item">
+      {props.user.name}
+    </td>
+
+    <td className="table-item">
+      {props.user.email}
+    </td>
+
+    <td className="table-item">
+      {props.user.service}
+    </td>
+    <td className="table-item">
+      <button
+        className="btn btn-remove"
+        onClick={() => props.handleUserRemove(props.user.id, props.user.name)}>
+        Remove book
+      </button>
+    </td>
+  </tr>
+)

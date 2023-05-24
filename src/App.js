8a4f0b20d1694a49e5c281fb7b32a95fa2217f30
leedsrising <leedsrising@gmail.com>
2023-05-23 21:46:55 -0400

    removing next.js and nextui to fix deployment

--- src/App.js
+++ src/App.js
@@ -3,23 +3,13 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes } from 'react-router-dom';
 
-import {NextUIProvider, createTheme} from '@nextui-org/react'
-
 import { Users } from './routes/adminUsersList';
-import { Home } from './routes/home';
-
-const darkTheme = createTheme({type: 'dark'})
-
-
 
 function App() {
   return (
-    <NextUIProvider theme={darkTheme}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/users" element={<Users />} />
-      </Routes>
-    </NextUIProvider>
+    <Routes>
+      <Route path="/" element={<Users />} />
+    </Routes>
   );
 }
 
